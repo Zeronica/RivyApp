81db819dcc4428f8efbec5d39a9c6c7580d48eb7
Ziran Nie <nieziran1996@gmail.com>
2015-03-06 18:22:03 -0800

    set up 4 new routes on rest

--- server/models/Rivys.js
+++ server/models/Rivys.js
@@ -2,7 +2,14 @@ var mongoose = require('mongoose');
 
 var RivySchema = new mongoose.Schema({
     title: String,
-    body: String
+    body: String,
+    upvotes: {type: Number, default: 0},
+  	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
-mongoose.model('Rivys', RivySchema);
+RivySchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+mongoose.model('Rivy', RivySchema);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,32 +2,63 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Rivys = mongoose.model('Rivys');
+var Rivy = mongoose.model('Rivy');
+var Comment = mongoose.model('Comment');
 
+// pre-loading objects
+router.param('rivy', function(req, res, next, id) {
+  var query = Rivy.findById(id);
 
+  query.exec(function (err, rivy){
+    if (err) { return next(err); }
+    if (!rivy) { return next(new Error('can\'t find rivy')); }
+
+    req.rivy = rivy;
+    return next();
+  });
+});
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+// view all rivys get /rivys
+// Add a new rivy post /rivys
+// upvote a rivy 
+// view comments associated with a rivy
+// add a comment to a rivy
+// upvote a comment to rivy
+
+// allow access
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index');
-// });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 
 /*GET list of rivys*/
 router.get('/rivys', function(req, res, next) {
-    Rivys.find(function(err, rivys){
+    Rivy.find(function(err, rivys){
         if (err){return next(err);}
-
         res.json(rivys);
     });
 });
 
 /*POST a new rivy*/
 router.post('/rivys', function(req, res, next) {
-  var rivy = new Rivys(req.body);
+  var rivy = new Rivy(req.body);
 
   rivy.save(function(err, rivy){
     if(err){ return next(err); }
@@ -35,5 +66,60 @@ router.post('/rivys', function(req, res, next) {
     res.json(rivy);
   });
 });
+
+/*GET a single rivy*/
+router.get('/rivys/:rivy', function(req, res) {
+  req.rivy.populate('comments', function(err, rivy) {
+    if (err) { return next(err); }
+
+    res.json(rivy);
+  });
+});
+
+/*upvote a single rivy*/
+router.put('/rivys/:rivy/upvote', function(req, res, next) {
+  req.rivy.upvote(function(err, rivy){
+    if (err) { return next(err); }
+
+    res.json(rivy);
+  });
+});
+
+/*upvote a single comment*/
+router.put('/rivys/:rivy/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+/*POST a comment*/
+router.post('/rivys/:rivy/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.rivy = req.rivy;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.rivy.comments.push(comment);
+    req.rivy.save(function(err, rivy) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/*upvote a comment*/
+
+// /*upvote a single rivy*/
+// router.put('/rivys/:rivy/upvote', function(req, res, next) {
+//   req.rivy.upvote(function(err, rivy){
+//     if (err) { return next(err); }
+
+//     res.json(rivy);
+//   });
+// });
 
 module.exports = router;
